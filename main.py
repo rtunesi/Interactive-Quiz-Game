bfcfee5241631a5f8969bbcad4ddf1a7bb5c73b3
Reece Tunesi <rtunesi2000@gmail.com>
2025-03-19 15:52:00 +0000

    Creation of project

---
+++ main.py
@@ -0,0 +1,9 @@
+# main coding file for the quiz game
+
+def getAnswer():
+    user_input = str(input("Please enter your answer: "))
+    return user_input
+
+
+print("Hello world!")
+print(getAnswer())
